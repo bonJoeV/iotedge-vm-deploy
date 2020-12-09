96db7012eeb86a95708fc2751cc19a2d5e546ef0
Joe Vandermark <joe.vandermark@gmail.com>
2020-12-09 11:49:32 -0600

    Create configure-device_connection_string.sh
    handle device string injection

---
+++ configure-device_connection_string.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#wait for the /etc/iotedge/config.yaml to be written
+while [ ! -f /etc/iotedge/config.yaml ]
+do
+  sleep 10
+done
+# File written, lets sleep a little longer to ensure no lock
+sleep 10
+# inject the connection string passed by variable dcs
+sudo sed -i "s#\(device_connection_string: \).*#\1\"$dcs\"#g" /etc/iotedge/config.yaml
+sudo systemctl restart iotedge
