41a6d3dca63369486aa1f1d975353beed2f1a1fe
Joe Vandermark <joe.vandermark@gmail.com>
2020-12-09 21:28:33 -0600

    Update configure-device_connection_string.sh

--- configure-device_connection_string.sh
+++ configure-device_connection_string.sh
@@ -1,14 +1,10 @@
-#! /bin/bash
-
-#wait for the /etc/iotedge/config.yaml to be written
-while [ ! -f /etc/iotedge/config.yaml ]
-do
-  sleep 10
-done
-# File written, lets sleep a little longer to ensure no lock
-sleep 60
-# inject the connection string passed by variable dcs
-sudo sed -i "s#\(device_connection_string: \).*#\1\"$dcs\"#g" /etc/iotedge/config.yaml
-echo $dcs >/etc/iotedge/config.done.txt
-sudo systemctl restart iotedge
-rm -f /etc/cron.d/configure-device_connection_string
+if [ -f /etc/iotedge/config.yaml ];
+then
+   sleep 10
+   # inject the connection string passed by variable dcs
+   sudo sed -i "s#\(device_connection_string: \).*#\1\"$dcs\"#g" /etc/iotedge/config.yaml
+   sudo systemctl restart iotedge
+   rm -f /etc/cron.d/configure-device_connection_string
+else
+   exit 0
+fi
