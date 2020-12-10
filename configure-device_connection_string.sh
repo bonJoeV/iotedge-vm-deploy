1178f2d7a61e6b10e10cc4eab3437d68477fb917
Joe Vandermark <joe.vandermark@gmail.com>
2020-12-09 21:33:45 -0600

    Update configure-device_connection_string.sh

--- configure-device_connection_string.sh
+++ configure-device_connection_string.sh
@@ -5,6 +5,7 @@ then
    sudo sed -i "s#\(device_connection_string: \).*#\1\"$dcs\"#g" /etc/iotedge/config.yaml
    sudo systemctl restart iotedge
    rm -f /etc/cron.d/configure-device_connection_string
+   unset dcs
 else
    exit 0
 fi
